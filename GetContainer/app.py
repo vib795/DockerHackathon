611126b2de64c2cf9a6049ee15f52e6cb4c3d436
Utkarsh Singh <utkarsh.singh0609@gmail.com>
2023-10-27 08:55:47 -0500

    Error fixes - get

--- GetContainer/app.py
+++ GetContainer/app.py
@@ -1,29 +1,14 @@
 import logging
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
-from data import check_and_trigger_alerts
+from get_service import setup_scheduler
 
 logging.basicConfig(filename='logger.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(threadName)-10s %('
-                                                                      'message)s',)
-
-# Initialize the scheduler
-scheduler = BackgroundScheduler()
-scheduler.start()
-
-def app():
-    scheduler.add_job(
-        check_and_trigger_alerts,
-        trigger=CronTrigger(second='0'),  # Runs every minute
-        id='check_due_reminders',
-        replace_existing=True
-    )
-
+                                                                      'message)s', )
 
 if __name__ == "__main__":
     try:
-        app()
+        setup_scheduler()
     except KeyboardInterrupt:
-        exit(0)
+        pass
     finally:
-        # Shut down the scheduler gracefully on program exit
-        scheduler.shutdown()
+        logging.info("Shutting down scheduler")
+        setup_scheduler.shutdown()
---
+++ GetContainer/get_service.py
@@ -0,0 +1,16 @@
+from data import check_and_trigger_alerts
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+
+def setup_scheduler(check_and_trigger_alerts):
+    scheduler = BackgroundScheduler()
+    scheduler.start()
+
+    # Schedule the job to check for due reminders and trigger alerts every minute
+    scheduler.add_job(
+        check_and_trigger_alerts,
+        trigger=CronTrigger(second='0'),  # Runs every minute
+        id='check_due_reminders',
+        replace_existing=True
+    )
